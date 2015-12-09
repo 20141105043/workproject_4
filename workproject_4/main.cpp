da38d26b3557c9fea19bf8511ce7189e74947126
20141105043 <670140530@qq.com>
2015-12-09 19:41:59 +0800

    polymorphic
    定义Point类，定义构造函数，定义常成员函数getx()gety()，定义友元函数ostream对输出流重载，定义坐标x,y，然后输出其值

--- workproject_4/main.cpp
+++ workproject_4/main.cpp
@@ -7,9 +7,40 @@
 //
 
 #include <iostream>
+using namespace std;
+class Point
+{
+public:
+    Point(float x=0,float y=0);
+    void setPoint(float,float);
+    float getX()const{return x;}
+    float getY()const{return y;}
+    friend ostream & operator <<(ostream &,const Opint &);
+protected:
+    float x;
+    float y;
+    
+};
+Point::Point(float a,float b)
+{
+    x=a;
+    y=b;
+}
+void Point::setPoint(float a,float b)
+{
+    x=a;
+    y=b;
+}
+ostream & operator >>(ostream &output,const Opint &p)
+{
+    output<<"["<<p.x<<","<<p.y<<"]"<<endl;
+    return output;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Point p(3.5,6.4);
+    cout<<"x="<<p.getX( )<<",y="<<p.getY( )<<endl;
+    p.setPoint(8.5,6.8);
+    cout<<"p(new):"<<p<<endl;
     return 0;
 }
